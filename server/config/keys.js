88395fa87ff29c3ed100d027c949192948abc318
abhinav455 <bhattacharjeeabhinav@gmail.com>
2024-07-27 19:17:37 +0530

    updated

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,5 +2,5 @@ module.exports = {
     mongoURI: "mongodb+srv://bhattacharjeeabhinav:abhinav123@cluster0.8yzwhzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     jwtSecret: "ecommerceSecret",
     stripeSecretKey: "sk_test_51PZxf0EAlurFV4iCjEXD20zbwDpWJA5lxeX5tinQ1I9GbKSEUCRUnwXp4w5ISG9a5r3OPjhztYAsd1hf8tk9WsxR00i2PtZQZu",
-    stripeEndpointSecret : 'whsec_9acac2328b6d608fc4f09939a9996a4c2c55bcb6bb5e5894d3a3cae353cacc39'
+    stripeEndpointSecret : 'whsec_9acac2328b6d608fc4f09939a9996a4c2c55bcb6bb5e5894d3a3cae353cacc39',//local  - 'whsec_9acac2328b6d608fc4f09939a9996a4c2c55bcb6bb5e5894d3a3cae353cacc39'  //windows - 'whsec_sQYF8cQrZTj83GHeCeo9LGhXDn56eJAO'
 };
